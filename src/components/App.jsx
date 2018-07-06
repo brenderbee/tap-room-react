8446a759581dce8f30d91ac27a9e227d32f663db
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-07-06 09:57:05 -0700

    Make App component stateful.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,37 +5,42 @@ import NewKegForm from './NewKegForm';
 import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
 
-function App(){
-  let mainStyles = {
-    margin: '0 10%'
-  };
-  return(
-    <div style={mainStyles}>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={KegList} />
-        <Route path='/newkeg' component={NewKegForm} />
-        <Route component={Error404} />
-      </Switch>
-      <style jsx global>{`
-        body {
-          font-family: 'Helvetica', sans-serif;
-          background: black;
-          color: white;
-          font-size: 1rem;
-        }
-        a {
-          text-decoration: none;
-        }
 
-        button {
-          font-size: 1rem;
-          text-transform: uppercase;
-          padding: 1.2rem;
-          color: white;
-        }
+class App extends React.Component {
+
+  render(
+    let mainStyles = {
+      margin: '0 10%'
+    };
+    return() {
+      <div style={mainStyles}>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={KegList} />
+          <Route path='/newkeg' component={NewKegForm} />
+          <Route component={Error404} />
+        </Switch>
+        <style jsx global>
+          {`
+            body {
+              font-family: 'Helvetica', sans-serif;
+              background: black;
+              color: white;
+              font-size: 1rem;
+            }
+            a {
+              text-decoration: none;
+            }
+
+            button {
+              font-size: 1rem;
+              text-transform: uppercase;
+              padding: 1.2rem;
+              color: white;
+            }
         `}</style>
-    </div>
+      </div>
+    }
   );
 }
 
